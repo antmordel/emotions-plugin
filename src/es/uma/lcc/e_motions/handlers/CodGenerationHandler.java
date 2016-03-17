9b08daee5a5fcdf8a36b866094e436568a5443cb
Antonio Moreno <amoreno@lcc.uma.es>
2016-03-17 15:49:45 +0100

    first import

---
+++ src/es/uma/lcc/e_motions/handlers/CodGenerationHandler.java
@@ -0,0 +1,20 @@
+package es.uma.lcc.e_motions.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class CodGenerationHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Helloworld",
+				"Hello, Eclipse world");
+		return null;
+	}
+}
